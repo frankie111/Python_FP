e78c1fb239a7177244c46711d1f7452b1a5c5b34
Mircox <mircea.rautoiu@gmail.com>
2022-11-14 22:06:33 +0200

    Complete ex3

---
+++ ex3/ex_3.py
@@ -0,0 +1,106 @@
+import random
+
+ascii_path = "ex3/rps_ascii.txt"
+
+
+def menu():
+    drawings_dict = load_ascii_drawings(ascii_path)
+    results = [0, 0]
+
+    for i in range(1, 4):
+        print(f"Runde {i}/3: " + ' ' * 20 + f"Scor Spieler-Computer: {results[1]} - {results[0]}")
+        result = rps_round(drawings_dict)
+
+        print(f"Runde {i}: ")
+        match result:
+            case 0:
+                print("Gleichheit!")
+            case 1:
+                print("Du hast verloren!")
+                results[0] += 1
+            case 2:
+                print("Du hast gewonnen!")
+                results[1] += 1
+
+    print()
+    if results[0] == results[1]:
+        print("Gleichheit! ")
+    elif results[0] > results[1]:
+        print("Du hast verloren!")
+    elif results[0] < results[1]:
+        print("Du hast gewonnen!")
+    print()
+
+
+def rps_round(drawings_dict):
+    """
+    Plays a round of Rock, Paper, Scissors and returns the result
+    :param drawings_dict:
+    :return:
+    """
+    computer_opt = get_random()
+    player_opt = input("Wähle aus: Schere(r), Stein(s) oder Papier(p): ")
+    print(drawings_dict[computer_opt])
+    result = get_winner(computer_opt, player_opt)
+    return result
+
+
+def get_winner(o1, o2):
+    """
+    Get the winner in Rock, Paper, Scissors between o1, o2:
+    :param o1: option 1
+    :type o1: str
+    :param o2: option 2
+    :type o2: str
+    :returns: 0 -> draw, 1 -> player1, 2 -> player2
+    """
+    if o1 == o2:
+        return 0
+
+    if o1 == 'r':
+        if o2 == 's':
+            return 1
+        else:
+            return 2
+
+    if o1 == 'p':
+        if o2 == 'r':
+            return 1
+        else:
+            return 2
+
+    if o1 == 's':
+        if o2 == 'p':
+            return 1
+        else:
+            return 2
+
+
+def get_random():
+    """
+    Returns a random choice from [r, p, s]
+    :return: 'r' or 'p' or 's' randomly
+    """
+    options = ['r', 'p', 's']
+    return random.choice(options)
+
+
+def load_ascii_drawings(path):
+    """
+    Reads ascii drawings from path and returns a dictionary with 3 pairs representing Rock, Paper and Scissors
+    :return:
+    """
+    file = open(path)
+    lines = file.readlines()
+    rps = []
+    sign = ""
+    for line in lines:
+        if line[0] != "*":
+            sign += line
+        else:
+            rps.append(sign)
+            sign = ""
+
+    drawings_dict = {'r': rps[0], 'p': rps[1], 's': rps[2]}
+
+    return drawings_dict
--- lab4.py
+++ lab4.py
@@ -1,11 +1,12 @@
 import ex1.ex_1
 import ex2.ex_2
+import ex3.ex_3
 
 
 def menu():
-    print("1. Draw character / word")
-    print("2. Replace words")
-    print("3. Rock, Paper, Scissors")
+    print("1. Zeichne Charakter / Wort")
+    print("2. Ersetze Wörter")
+    print("3. Schere, Stein, Papier (best of 3)")
 
     option = int(input("Your option: "))
     match option:
@@ -14,6 +15,9 @@ def menu():
             menu()
         case 2:
             ex2.ex_2.menu()
+            menu()
+        case 3:
+            ex3.ex_3.menu()
             menu()
         case _:
             print(str(option) + " is not a valid option!")
